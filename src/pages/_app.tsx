905d5b624b5d44cc195dc4767dc1132ad1ed989e
JuniorTunarr <shclgus111@gmail.com>
2023-02-12 22:01:35 +0900

    3

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,5 +3,16 @@ import type { AppProps } from "next/app";
 import { useEffect } from "react";
 
 export default function App({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    if ("serviceWorker" in navigator) {
+      const registInit = async () => {
+        const registration = await navigator.serviceWorker.register(
+          "/service-wroker.js"
+        );
+        registration.waiting?.postMessage("SKIP_WAITING");
+      };
+      registInit();
+    }
+  });
   return <Component {...pageProps} />;
 }
