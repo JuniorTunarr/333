e47749d88316478cd4dc3bcfb074a052a66dda83
JuniorTunarr <shclgus111@gmail.com>
2023-02-12 20:39:45 +0900

    2

--- next.config.js
+++ next.config.js
@@ -1,6 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+};
 
-module.exports = nextConfig
+module.exports = withPlugins(
+  [
+    [
+      withPWA,
+      {
+        pwa: {
+          dest: "public",
+          publicExcludes: ["!robots.txt", "!sitemap.xml.gz"],
+        },
+      },
+    ],
+    // 추가 플러그인 작성
+  ],
+  nextConfig
+);
