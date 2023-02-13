0b071fe4390c1c4d34312f5494dff80dc88409d8
JuniorTunarr <shclgus111@gmail.com>
2023-02-13 09:25:09 +0900

    3

--- next.config.js
+++ next.config.js
@@ -1,11 +1,23 @@
 /** @type {import('next').NextConfig} */
+const withPlugins = require("next-compose-plugins");
 const withPWA = require("next-pwa");
 
-module.exports = withPWA({
-  target: "serverless",
-  pwa: {
-    dest: "public",
-    disable: process.env.NODE_ENV === "development",
-  },
-  // ... whatever other config you might have
-});
+const nextConfig = {
+  reactStrictMode: true,
+};
+
+module.exports = withPlugins(
+  [
+    [
+      withPWA,
+      {
+        pwa: {
+          dest: "public",
+          disable: process.env.NODE_ENV === "development",
+        },
+      },
+    ],
+    // 추가 플러그인 작성
+  ],
+  nextConfig
+);
